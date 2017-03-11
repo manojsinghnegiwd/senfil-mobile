cc04bc3afad3a65aae2d2badd8b45812610dcb8c
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-03-11 16:08:16 +0530

    By default show Scan Screen

--- index.android.js
+++ index.android.js
@@ -11,21 +11,13 @@ import {
   Text,
   View
 } from 'react-native';
+import ScanScreen from './containers/scan.js';
 
 export default class mobile extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <ScanScreen />
       </View>
     );
   }
