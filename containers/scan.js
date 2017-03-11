a7da027dacc72dbb5fc49fa4bb330e236ffaec74
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-03-11 16:22:20 +0530

    added props to Button

--- containers/scan.js
+++ containers/scan.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
 	View,
+	Text
 } from 'react-native';
 
 import {Button} from '../components'
@@ -14,7 +15,9 @@ export default class ScanScreen extends Component {
 	render () {
 		return (
 			<View>
-				<Button onPress={this._onPressButton} />
+				<Button isRipple onPress={this._onPressButton} style={{ width: 100, height: 50}} rippleColor="red">
+					<Text> Button </Text>
+				</Button>
 			</View>
 		)
 	}
