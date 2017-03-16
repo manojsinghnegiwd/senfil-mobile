1139d563549286c03bdae719673fbd1cb02a62de
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-03-16 22:46:30 +0530

    fixed upload button layout

--- containers/upload.js
+++ containers/upload.js
@@ -22,7 +22,7 @@ export default class UploadScreen extends Component {
 let styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     padding: 10
   }
 });
